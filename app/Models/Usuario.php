64154c98e1ec90a45986fbb9461f655ecd369282
mercer2511 <98627279+mercer2511@users.noreply.github.com>
2024-12-19 23:10:54 -0500

    Eliminar uso de traits innecesarios en el modelo Usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,12 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class Usuario extends Model
 {
-    use HasApiTokens, Notifiable;
 
     protected $table = 'usuarios';
 

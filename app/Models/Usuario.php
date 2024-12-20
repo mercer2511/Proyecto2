458cec55b06f9abf92d1d90fd9d0b31f9c25ee87
mercer2511 <98627279+mercer2511@users.noreply.github.com>
2024-12-19 23:16:33 -0500

    Actualizar modelo Usuario para usar autenticación de Sanctum y agregar configuración de Sanctum; crear migración para tokens de acceso personal

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Usuario extends Model
+class Usuario extends Authenticatable
 {
-    #use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable;
 
     protected $table = 'usuarios';
     protected $keyType = 'string';

1cf70862a482fb9cba4ef037337f95f5f6df371e
mercer2511 <98627279+mercer2511@users.noreply.github.com>
2024-12-20 18:47:00 -0500

    Mejorar la alineación de texto en las tablas de las vistas de grados y cursos; actualizar enlace en el panel de administrador y ajustar el ancho del contenedor en la vista de docentes.

--- resources/views/cargos/administradorindex.blade.php
+++ resources/views/cargos/administradorindex.blade.php
@@ -6,7 +6,7 @@
     <h1 class="text-center py-5">panel de la secretaria</h1>
 
     <div class="grid justify-items-center ">
-        <a class="m-1 p-3 rounded-md bg-[#e70505] btn btn-secondary " href="{{ route('vistaPrincipal') }}"> Nuevos registros </a>   
+        <a class="m-1 p-3 rounded-md bg-[#e70505] btn btn-secondary " href="{{ route('AdministradorListar') }}"> Nuevos registros </a>   
     </div>
 </div>
 

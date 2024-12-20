2b84ed38d76b427746f7036bf7e70a0f9b9963fc
OLIVERFRANCISCO <2301010004@undc.edu.pe>
2024-12-19 20:48:55 -0500

    Agregar ruta para la vista 'about'

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
 
 Route::get('/', function () {
     return view('index');
 });
 
-
+Route::get('/about', function () {
+    return view('about');
+});
 
